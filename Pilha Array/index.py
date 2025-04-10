044ad0fe180959106520c12e166fc43271538090
Matheus William Joenck <117489072+al0i@users.noreply.github.com>
2025-04-10 11:23:25 -0300

    Final version

--- Pilha Array/index.py
+++ Pilha Array/index.py
@@ -2,36 +2,38 @@
 
 class No:
     def __init__(self,value):
-        self.value = value
+        self.__value = value
+
+    def get_value(self):
+        return self.__value
 
 class Pilha:            
     def __init__(self, lim):
-        self.topo = -1
-        self.array = []
-        self.lim = lim
-        self.lenght = self.get_lenght()
-        self.cab = self.get_cab() #Cabeçalho, que será o primeiro valor da pilha
+        self.__top = -1
+        self.__array = []
+        self.__lim = lim
+        self.__head = self.get_cab()
 
-    #Getters padrões
+    #Getters padrões:
     def get_topo(self):
-        return self.topo
+        return self.__top
     
     def get_cab(self):
         if self.get_lenght() == 0:
             return None
-        return self.array[0].value
+        return self.__array[0].get_value()
     
     def get_lenght(self):
-        return self.topo + 1
+        return self.__top + 1
 
-    #Retorna os valores contidos na pilha (usei durante o desenvolvimento do programa)
+    #Retorna os valores contidos na pilha (usei durante o desenvolvimento do programa):
     def get_values(self):
         retorno = ""
-        for no in self.array:
-            retorno = retorno+str(no.value)+" "
+        for no in self.__array:
+            retorno = retorno+str(no.get_value())+" "
         return retorno
     
-    #Mostra na tela dados gerais da pilha (usei durante o desenvolvimento do programa)
+    #Mostra na tela dados gerais da pilha (usei durante o desenvolvimento do programa):
     def dados(self):
         print(f"""
             Topo: {self.get_topo()}
@@ -40,67 +42,56 @@ class Pilha:
             Pilha: {self.get_values()}
 """)
 
-    #Método de adição de valores à pilha
+    #Método de adição de valores à pilha:
     def add(self,value):
-        if self.get_lenght() == self.lim:
+        if self.get_lenght() == self.__lim:
             return False
         
-        no = No(value)
-        self.array += [no]
-        self.topo += 1
-        return True
-        
-        
-    #Método de remoção de valores da pilha, que segue o padrão Last-In First-Out
+        elif self.get_lenght() == 0:
+            no = No(value)
+            self.__array = [no]
+            self.__top += 1
+            return True
+
+        else:
+            no = No(value)
+            newArray = [None] * (self.get_lenght()+1)
+            for i in range(self.get_lenght()):
+                newArray[i] = self.__array[i]
+            newArray[self.get_lenght()] = no
+            self.__array = newArray
+            self.__top = self.__top+1
+            return True
+
+    #Método de remoção de valores da pilha, que segue o padrão Last-In First-Out:
     def drop(self):
         if self.get_lenght() == 0:
             return False #Pilha vazia
         
-        elif self.get_lenght() == 1:
-            self.cab = None
-            self.topo -= 1
-            self.array = [x for x in self.array if x != self.array[-1]]
-            return True
-        
         else:
-            arrayLenght = self.get_lenght()
-            newArray = []
-            for i in range(arrayLenght-1):
-                newArray = newArray + [self.array[i]]#VERIFICAR!
-            self.topo -= 1
-            self.array = newArray
-            return True    
+            newArray = [None] * (self.get_lenght()-1)
+            for i in range(self.get_lenght()-1):
+                newArray[i] = self.__array[i]
+            self.__array = newArray
+            self.__top = self.__top-1
+            return True
     
-#Função usada para testar o código
+#Função usada para testar o código (como um main()):
 def testar():
     pilha = Pilha(5)
 
-
-    pilha.get_lenght()
     pilha.dados()
+    
 
-    pilha.add(1)
-    pilha.dados()
+    for i in range(1,7):
+        
+        if not pilha.add(i):
+            print("PILHA CHEIA!")
+        pilha.dados()
 
-    pilha.add(2)
-    pilha.dados()
-
-    pilha.add(3)
-    pilha.dados()
-
-    pilha.add(4)
-    pilha.dados()
-
-    pilha.drop()
-    pilha.dados()
-
-    pilha.drop()
-    pilha.dados()
-
-    pilha.drop()
-    pilha.dados()
-
-    pilha.drop()
-    pilha.dados()
+    for i in range(pilha.get_lenght()+1):
+        if not pilha.drop():
+            print("PILHA VAZIA!")
+        pilha.dados()
 
 testar()
